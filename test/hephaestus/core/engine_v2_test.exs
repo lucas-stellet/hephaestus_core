e80b3b344f8fcbdae06d2d8b8487d56fe81135ca
Lucas Stellet <lustepe@gmail.com>
2026-04-05 23:20:44 -0300

    test: add engine v2 flow coverage

--- test/hephaestus/core/engine_v2_test.exs
+++ test/hephaestus/core/engine_v2_test.exs
@@ -1,3 +1,37 @@
+defmodule Hephaestus.Test.V2.EngineBranchWorkflow do
+  use Hephaestus.Workflow
+
+  @impl true
+  def start, do: Hephaestus.Test.V2.BranchStep
+
+  @impl true
+  def transit(Hephaestus.Test.V2.BranchStep, :approved), do: Hephaestus.Test.V2.ApproveStep
+  def transit(Hephaestus.Test.V2.BranchStep, :rejected), do: Hephaestus.Test.V2.RejectStep
+  def transit(Hephaestus.Test.V2.ApproveStep, :done), do: Hephaestus.Steps.End
+  def transit(Hephaestus.Test.V2.RejectStep, :done), do: Hephaestus.Steps.End
+end
+
+defmodule Hephaestus.Test.V2.EngineDynamicWorkflow do
+  use Hephaestus.Workflow
+
+  @impl true
+  def start, do: Hephaestus.Test.V2.StepA
+
+  @impl true
+  @targets [Hephaestus.Test.V2.StepB, Hephaestus.Test.V2.StepC]
+  def transit(Hephaestus.Test.V2.StepA, :done, ctx) do
+    if ctx.initial.use_b do
+      Hephaestus.Test.V2.StepB
+    else
+      Hephaestus.Test.V2.StepC
+    end
+  end
+
+  @impl true
+  def transit(Hephaestus.Test.V2.StepB, :done), do: Hephaestus.Steps.End
+  def transit(Hephaestus.Test.V2.StepC, :done), do: Hephaestus.Steps.End
+end
+
 defmodule Hephaestus.Core.EngineV2Test do
   use ExUnit.Case, async: true
 
@@ -135,6 +169,70 @@ defmodule Hephaestus.Core.EngineV2Test do
       activated = Engine.activate_transitions(instance, Hephaestus.Test.V2.StepA, :done)
 
       assert activated.step_configs[Hephaestus.Test.V2.ConfigStep] == %{timeout: 1000}
+    end
+  end
+
+  describe "full linear flow without OTP" do
+    test "advances through A -> B -> End entirely in pure functions" do
+      instance = Instance.new(Hephaestus.Test.V2.LinearWorkflow, %{data: "test"})
+
+      {:ok, inst} = Engine.advance(instance)
+      assert inst.status == :running
+      assert MapSet.member?(inst.active_steps, Hephaestus.Test.V2.StepA)
+
+      {:ok, event} = Engine.execute_step(inst, Hephaestus.Test.V2.StepA)
+      assert event == :done
+
+      inst = Engine.complete_step(inst, Hephaestus.Test.V2.StepA, :done, %{})
+      inst = Engine.activate_transitions(inst, Hephaestus.Test.V2.StepA, :done)
+      assert MapSet.member?(inst.active_steps, Hephaestus.Test.V2.StepB)
+
+      {:ok, event, ctx} = Engine.execute_step(inst, Hephaestus.Test.V2.StepB)
+      assert event == :done
+
+      inst = Engine.complete_step(inst, Hephaestus.Test.V2.StepB, :done, ctx)
+      inst = Engine.activate_transitions(inst, Hephaestus.Test.V2.StepB, :done)
+      assert MapSet.member?(inst.active_steps, Hephaestus.Steps.End)
+
+      {:ok, event} = Engine.execute_step(inst, Hephaestus.Steps.End)
+      assert event == :end
+
+      inst = Engine.complete_step(inst, Hephaestus.Steps.End, :end, %{})
+      inst = Engine.check_completion(inst)
+
+      assert inst.status == :completed
+    end
+  end
+
+  describe "full branch flow without OTP" do
+    test "takes approved branch" do
+      instance = Instance.new(Hephaestus.Test.V2.EngineBranchWorkflow, %{approved: true})
+
+      {:ok, inst} = Engine.advance(instance)
+      {:ok, event} = Engine.execute_step(inst, Hephaestus.Test.V2.BranchStep)
+
+      assert event == :approved
+
+      inst = Engine.complete_step(inst, Hephaestus.Test.V2.BranchStep, :approved, %{})
+      inst = Engine.activate_transitions(inst, Hephaestus.Test.V2.BranchStep, :approved)
+
+      assert MapSet.member?(inst.active_steps, Hephaestus.Test.V2.ApproveStep)
+      refute MapSet.member?(inst.active_steps, Hephaestus.Test.V2.RejectStep)
+    end
+  end
+
+  describe "dynamic transit/3 flow" do
+    test "resolves transit/3 using context" do
+      instance = Instance.new(Hephaestus.Test.V2.EngineDynamicWorkflow, %{use_b: true})
+
+      {:ok, inst} = Engine.advance(instance)
+      {:ok, :done} = Engine.execute_step(inst, Hephaestus.Test.V2.StepA)
+      inst = Engine.complete_step(inst, Hephaestus.Test.V2.StepA, :done, %{})
+
+      inst = Engine.activate_transitions(inst, Hephaestus.Test.V2.StepA, :done)
+
+      assert MapSet.member?(inst.active_steps, Hephaestus.Test.V2.StepB)
+      refute MapSet.member?(inst.active_steps, Hephaestus.Test.V2.StepC)
     end
   end
 end
